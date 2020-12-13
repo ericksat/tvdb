0b9d189a28d8b2e0a5965e50895d7acd7d12c2f8
Erick.S <esatire@gmail.com>
2020-12-13 08:21:57 +0200

    Updated client code (PWA)

---
+++ dist/precache-manifest.38d86f7cb19ed0ddaf7be3dd9f235971.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "583a82683dedca1ee315",
+    "url": "/css/app.0f97a5bd.css"
+  },
+  {
+    "revision": "762b635817d2f177c3f277f93c56ba55",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "c6bb8c5993eb5b99381eaac38a23bede",
+    "url": "/index.html"
+  },
+  {
+    "revision": "583a82683dedca1ee315",
+    "url": "/js/app.d123e2da.js"
+  },
+  {
+    "revision": "754e696d9d67578c440b",
+    "url": "/js/chunk-vendors.9b915e95.js"
+  },
+  {
+    "revision": "dc7a1c13cb3b1c06855d506cc451d1ac",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
